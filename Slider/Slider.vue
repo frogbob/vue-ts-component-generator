c524a061c10cf46830ede028df841da8b4497c37
Freerk van Zeijl <freerk.vanzeijl@ultrabold.com>
2018-01-16 17:23:32 +0100

    Remove unused dummy folder

--- Slider/Slider.vue
+++
@@ -1,6 +0,0 @@
-<template src="./Slider.html"></template>
-
-<script lang="ts" src="./Slider.ts"></script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" src="./Slider.scss" scoped></style>
