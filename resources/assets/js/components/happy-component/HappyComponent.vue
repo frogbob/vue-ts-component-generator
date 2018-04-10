4ed2e619de35fbe5a639746d6d648ca328fdf69b
Freerk van Zeijl <freerk.vanzeijl@ultrabold.com>
2018-04-10 12:00:54 +0200

    Removed dummy-component.

--- resources/assets/js/components/happy-component/HappyComponent.vue
+++
@@ -1,6 +0,0 @@
-<template src="./HappyComponent.html"></template>
-
-<script lang="ts" src="./HappyComponent.ts"></script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" src="./HappyComponent.scss" scoped></style>
